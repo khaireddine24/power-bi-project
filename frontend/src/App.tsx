d1d338a7baa5933782a6cccd569fdde20dc19dd7
Khaireddine Ihrissane <ihrissanek@gmail.com>
2025-04-13 18:34:30 +0100

    Add frontend part

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import {Button} from "./components/ui/button";
+function App() {
+
+  return (
+    <>
+    <h1 className="text-2xl font-bold text-red-500">Front bi part</h1>
+    <Button variant={"destructive"}>Click Me</Button>
+    </>
+  )
+}
+
+export default App
